6529a2c8924f2dc5e6b748d9595011efcfb9f1ed
Jireii <mazhn34@gmail.com>
2021-12-05 22:10:05 +0800

    fix InputController & web path

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -8,31 +8,34 @@ use App\Http\Controllers\Controller;
 
 class InputController extends Controller
 {
-    // List
-    public function index()
-    {
-        return view('input.index', [
-            'one_inputs' => OneInput::all(),
-        ]);
-    }
-
     // Admin
-    // Detail
-    public function admin_detail()
-    {
-    }
-    // Create
-    public function admin_create()
-    {
-    }
-    // Edit
-    public function admin_edit()
-    {
-    }
-    // Delete
-    public function admin_delete()
-    {
-    }
+        // Index
+        public function admin_index()
+        {
+            return view('input.admin.index', [
+                'one_inputs' => OneInput::all(),
+            ]);
+        }
+        // Detail
+        public function admin_detail()
+        {
+
+        }
+        // Create
+        public function admin_create()
+        {
+
+        }
+        // Edit
+        public function admin_edit()
+        {
+
+        }
+        // Delete
+        public function admin_delete()
+        {
+
+        }
 
     // Common
     // Detail
