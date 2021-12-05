3042123e0ddf49e947b2da59ce3735eb3a88a476
revaldy-30 <rejarevaldy30@gmail.com>
2021-12-05 21:59:43 +0800

    Index admin

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\Controller;
 
 class InputController extends Controller
 {
-<<<<<<< HEAD
     // List
     public function index()
     {
@@ -52,72 +51,4 @@ class InputController extends Controller
     public function common_delete()
     {
     }
-=======
-
-    // Admin
-        // Index
-        public function admin_index(){
-            return view('input.index');
-        }
-        // Detail
-        public function admin_detail(){
-
-        }
-        // Store
-        public function admin_store(){
-
-        }
-        // Edit
-        public function admin_edit(){
-
-        }
-        // Destroy
-        public function admin_destroy(){
-
-        }
-
-    // Common
-        // Detail
-        public function common_detail(){
-
-        }
-        // Store
-        public function common_store(){
-
-        }
-        // Edit
-        public function common_edit(){
-
-        }
-        // Destroy
-        public function common_destroy(){
-
-        }
-
-    // Umum
-        // Index
-        public function umum_index(){
-
-        }
-    // PPA I
-        // Index
-        public function PPAI_index(){
-
-        }
-    // PPA II
-        // Index
-        public function PPAII_index(){
-
-        }
-    // SKKI
-        // Index
-        public function SKKI_index(){
-
-        }
-    // PAPK
-        // Index
-        public function PAPK_index(){
-
-        }
->>>>>>> 2eba28e7f8c7149de3781268745620d2a11ef38d
 }
