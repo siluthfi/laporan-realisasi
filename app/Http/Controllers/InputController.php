d90ac95db199e8ef0c03ea9534f7aa644fdf4979
Jireii <mazhn34@gmail.com>
2021-12-05 21:30:43 +0800

    updated input controller

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -6,7 +6,44 @@ use Illuminate\Http\Request;
 
 class InputController extends Controller
 {
+    // List
     public function index(){
         return view('input.index');
     }
+
+    // Admin
+        // Detail
+        public function admin_detail(){
+
+        }
+        // Create
+        public function admin_create(){
+
+        }
+        // Edit
+        public function admin_edit(){
+
+        }
+        // Delete
+        public function admin_delete(){
+
+        }
+
+    // Common
+        // Detail
+        public function common_detail(){
+
+        }
+        // Create
+        public function common_create(){
+
+        }
+        // Edit
+        public function common_edit(){
+
+        }
+        // Delete
+        public function common_delete(){
+
+        }
 }
